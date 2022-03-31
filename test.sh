a8dcb060b5fc01a88928db8d4b1d3a9670e4a990
Dennis Magnusson <dennis.magnusson98@gmail.com>
2022-03-31 15:52:19 +0200

    Move files after done

--- test.sh
+++ test.sh
@@ -1,1 +1,5 @@
-python3 test.py --preprocess scale_longside --dataroot ../lr_dataset/ --name cityscapes_cut_pretrained --model cut  --no_dropout --preprocess scale_longside --load_size 512 
+# TODO Replace cityscapes_cut_pretrained with train model name
+
+python3 test.py --preprocess scale_longside --dataroot ../lr_dataset/ --name cityscapes_cut_pretrained --model cut  --no_dropout --preprocess scale_longside --load_size 512 --gpu_ids -1
+
+mv ./results/cityscapes_cut_pretrained/test_latest/images/ ../results/
